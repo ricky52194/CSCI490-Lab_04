2fa4c74dd8f1393dfcb2e05d0a574811fe01fdb9
ricky52194 <ramosrs@g.cofc.edu>
2018-03-08 18:08:41 -0500

    webview lab

--- app/src/main/java/com/example/ricky52194/csci490_lab_04/MainActivity.java
+++ app/src/main/java/com/example/ricky52194/csci490_lab_04/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.ricky52194.csci490_lab_04;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        WebView webview = new WebView(this);
+        setContentView(webview);
+        webview.loadUrl("https://www.google.com/");
     }
 }
