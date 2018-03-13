225be75951b7378b3bbec80e6c23d9348e5a6524
ricky52194 <ramosrs@g.cofc.edu>
2018-03-13 16:57:27 -0400

    async task

--- app/src/main/java/com/example/ricky52194/csci490_lab_04/MainActivity.java
+++ app/src/main/java/com/example/ricky52194/csci490_lab_04/MainActivity.java
@@ -2,68 +2,23 @@ package com.example.ricky52194.csci490_lab_04;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLConnection;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        getURL("https://jsonplaceholder.typicode.com/users");
+
+        TextView view= findViewById(R.id.webview);
+
+        myAsyncTask task = new myAsyncTask(view);
+        task.execute("https://jsonplaceholder.typicode.com/users");
+
+
     }
 
-
-    public String getURL(String s)  {
-
-
-
-        URL url = null;
-        try {
-            url = new URL(s);
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        }
-
-
-
-        URLConnection connection = null;
-        try {
-            connection = url.openConnection();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-
-        InputStreamReader isr = null;
-        try {
-            isr = new InputStreamReader((connection.getInputStream()));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-
-
-        BufferedReader in = new BufferedReader(isr);
-        String line = null;
-        try {
-            line = in.readLine();
-            while (in.readLine() != null) {
-                Log.i("Okay", line);
-            }
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-
-        return s;
-    }
 }
---
+++ app/src/main/java/com/example/ricky52194/csci490_lab_04/myAsyncTask.java
@@ -0,0 +1,79 @@
+package com.example.ricky52194.csci490_lab_04;
+
+import android.os.AsyncTask;
+import android.widget.TextView;
+import android.util.Log;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
+/**
+ * Created by ricky52194 on 3/13/18.
+ */
+
+public class myAsyncTask extends AsyncTask<String, Void, String> {
+
+    private TextView textview;
+
+
+    public myAsyncTask(TextView textView) {
+        this.textview = textView;
+    }
+
+    @Override
+    protected String doInBackground (String... String){
+        URL url = null;
+        try {
+            url = new URL(String[0]);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        URLConnection connection = null;
+        try {
+            connection = url.openConnection();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        InputStreamReader inputStreamReader = null;
+        try {
+            inputStreamReader = new InputStreamReader((connection.getInputStream()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+        String line = null;
+
+
+        try {
+            line = bufferedReader.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        int i = 0;
+        while(i < 20){
+            try {
+                line += bufferedReader.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            i++;
+        }
+
+        return line;
+    }
+
+    @Override
+    protected void onPostExecute(String string) {
+        textview.setText(string.substring(10, 23));
+
+    }
+}
